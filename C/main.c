96923b52ba884c62f2e1c124204db41ba498b3f9
Davi Oliveira <davioliveira541@outlook.com>
2024-11-19 19:14:49 -0300

    UPDATES ON MAIN

--- C/main.c
+++ C/main.c
@@ -5,7 +5,13 @@
 
 
 const MAP_SIZE = 21;
+const BOMB_TIMER = 3;
 
+//Directions
+const UP = 0;
+const DOWN = 1;
+const LEFT = 2;
+const RIGHT = 3;
 
 // Cell Types
 const EMPTY_CELL = 0;
@@ -15,9 +21,16 @@ const BOMB = 3;
 const ITEM = 4;
 const PLAYER1 = 5;
 const PLAYER2 = 6;
+
+//Game Stats
+const ONGOING = 0;
+const DRAW = 1;
+const P1WINS = 2;
+const P2WINS = 3;
+
 typedef union  Object
 {
-    Block* block;
+    Block block;
     Item item;
     Player player;
     Bomb bomb;
@@ -80,55 +93,149 @@ Map* generateMap(int layout[MAP_SIZE][MAP_SIZE]) {
 
                 case BLOCK:
                     map->matriz[i][j].type = BLOCK;
-                    map->matriz[i][j].object.block = (Block*) malloc(sizeof(Block));
-
-                    map->matriz[i][j].object.block->HP = 1;
+                    Block* block = &map->matriz[i][j].object.block;
+                    block->HP = 1;
                     break;
 
                 case BARRIER:
                     map->matriz[i][j].type = BARRIER;
-                    map->matriz[i][j].object.block = (Block*) malloc(sizeof(Block));
-
-                    map->matriz[i][j].object.block->HP = 1;
+                    Block* block = &map->matriz[i][j].object.block;
+                    block->HP = 1;
                     break;
             }
         }
     }
-
     return map;
 }
 
-bool takeDamage(Cell cell)
+void explode(Bomb bomb)
 {
-    switch (cell.type)
+    //TODO
+}
+
+bool takeDamage(Cell* cell)
+{
+    switch (cell->type)
     {
         case EMPTY_CELL:
             return false;
         case BLOCK:
-            cell.object.block->HP -=1;
-            if(cell.object.block->HP == 0)
+            cell->object.block.HP -=1;
+            if(cell->object.block.HP == 0)
             {
-                cell.type = EMPTY_CELL;
-                cell.object.empty = EMPTY_CELL;
+                cell->type = EMPTY_CELL;
+                cell->object.empty = EMPTY_CELL;
             }
             return true;
         case BARRIER:
             return true;
         case BOMB:
-            cell.object.bomb.timer = 0;
-            explode(cell.object.bomb);
+            cell->object.bomb.timer = 0;
+            explode(cell->object.bomb);
             break;
         default:
-            cell.object.player.HP -=1;
-            if(cell.object.player.HP == 0)
+            cell->object.player.HP -=1;
+            if(cell->object.player.HP == 0)
             {
-                cell.type = EMPTY_CELL;
-                cell.object.empty = EMPTY_CELL;
+                cell->type = EMPTY_CELL;
+                cell->object.empty = EMPTY_CELL;
             }
             return true;
     }
-
     return false;
+}
+
+void damagePlayer(Player player, int x,int y)
+{
+    if((x==player.posX) && (y == player.posY))
+    {
+        player.HP -=1;
+    }
+}
+
+bool movePlayer(Map map, Player player, int direction)
+{
+    switch (direction)
+    {
+        case UP:
+            if(map.matriz[player.posX-1][player.posY].type == EMPTY_CELL)
+            {
+                player.facingDirection = UP;
+                player.posX -= 1;
+                return true;
+            }
+        case DOWN:
+            if (map.matriz[player.posX+1][player.posY].type == EMPTY_CELL)
+            {
+                player.facingDirection = DOWN;
+                player.posX += 1;
+                return true;
+            }
+        case LEFT:
+            if (map.matriz[player.posX][player.posY-1].type == EMPTY_CELL)
+            {
+                player.facingDirection = LEFT;
+                player.posY -= 1;
+                return true;
+            }
+        case RIGHT:
+            if (map.matriz[player.posX][player.posY+1].type == EMPTY_CELL)
+            {
+                player.facingDirection = RIGHT;
+                player.posY += 1;
+                return true;
+            }
+    }
+    return false;
+}
+
+bool placeBomb(Player* player, Map* map)
+{
+    bool bombPlaced = false;
+    if(player->bombs>0)
+    {
+        Bomb* bomb = &map->matriz[player->posX][player->posY].object.bomb;
+        bomb->timer = BOMB_TIMER;
+        bomb->power = player->power;
+        bomb->owner = *player;
+
+        map->matriz[player->posX][player->posY].type = BOMB;
+
+        bombPlaced = true;
+    }
+    if(bombPlaced)
+    {
+        player->bombs -=1;
+    }
+    return bombPlaced;
+}
+
+int updateGame(Map* map, Player* player1, Player* player2)
+{
+    int i, j;
+    for(i=0; i<MAP_SIZE; i++)
+    {
+        for(j=0; j<MAP_SIZE; j++)
+        {
+            map->x = i;
+            map->y = j;
+            if(map->matriz[i][j].type == BOMB)
+            {
+                explode(map->matriz[i][j].object.bomb);
+            }
+        }
+    }
+
+    if(player1->HP<1 && player2->HP<1)
+        return DRAW;
+    else if(player2->HP<1)
+        return P1WINS;
+    else if(player1->HP<1)
+        return P2WINS;
+    else
+        return ONGOING;
+
+
 }
 int main()
 {
