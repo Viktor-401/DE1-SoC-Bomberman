56f84c837d5b1a57f5f42a58bad00e3829a42b10
Silva-Alisson <112671617+Silva-Alisson@users.noreply.github.com>
2024-11-23 16:15:10 -0300

    Test Sprites
    falta fazer uma pequena mudança pra a função receber a matriz.

---
+++ C/testNewSprites.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+#include "config.c"
+#include "gpu_lib.h"
+#include "accel_lib.h"
+
+
+void desenhar_quadrado_na_gpu(unsigned long sprite_slot) {
+    // Matriz 20x20 representando o sprite (valores de 0 a 5)
+    unsigned long matriz_sprite[20][20] = {
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    };
+
+    // Tabela de cores para os números da matriz
+    int tabela_cores[6] = {
+    0b000000000,   // 0: Preto ou transparente
+    0b111000000, // 2: Vermelho
+    0b000000111, // 1: Azul
+    0b110011001, // 3: Verde
+    0b010101101, // 4: Amarelo
+    0b001010110  // 5: Roxo
+    };
+
+
+    // Enviar os pixels para a GPU
+    unsigned long y =0;
+    unsigned long x = 0;
+    for (y = 0; y < 20; y++) {
+        for (x = 0; x < 20; x++) {
+            set_sprite_memory(1, tabela_cores[matriz_sprite[y][x]], x, y);
+        }
+    }
+}
+
+int main() {
+    unsigned long sprite_slot = 2;  // Usaremos o slot 0 para o sprite
+    create_mapping_memory();
+    clear_all();
+    desenhar_quadrado_na_gpu(sprite_slot);
+    set_background_color(0,7,0);
+    //set_sprite(1,0,1,50,50);
+    set_sprite(2,1,1,110,100);
+    
+    return 0;
+}
